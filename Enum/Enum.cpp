1ad3bc7525d74d08018b6c38883833836402fc63
gasshot <kanginoh@gmail.com>
2026-01-16 16:35:27 +0900

    260116 1635

---
+++ Enum/Enum.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+enum ActorState
+{
+	Idle,
+	Walk,
+	Run,
+	Sprint
+};
+
+int main() 
+{
+	enum ActorState actorstate;
+
+	switch (actorstate)
+	{
+	case Idle:
+		printf("지금은 대기상태입니다.");
+		break;
+	case Walk:
+		printf("지금은 걷기상태입니다.");
+		break;
+	case Run:
+		printf("지금은 달리기상태입니다.");
+		break;
+	case Sprint:
+		printf("지금은 전력질주상태입니다.");
+		break;
+	default:
+		break;
+	}
+
+
+	return 0;
+}
