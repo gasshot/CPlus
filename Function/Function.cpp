7850373b4547d370228a238a1d8b6677bd436c86
강동윤 <kanginoh@gmail.com>
2026-01-26 16:30:23 +0900

    260126 1630

---
+++ Function/Function.cpp
@@ -0,0 +1,63 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int add_Number(int* parr);
+int print_Hello() 
+{
+	printf("Hello!!\n");
+	return 500;
+	printf("안보임!!\n");
+}
+
+int returnNum(int inputNum) 
+{
+	// 입력값, 출력(반환)값 함수
+	// 입력값에 수정을 가한 후 입력값을 반환
+	inputNum += 10;
+	return inputNum;
+}
+
+int add(int num)
+{
+	printf("%d\n", num);
+	return num;
+}
+
+char add(char num)
+{
+	printf("%c\n", num);
+	return num;
+}
+
+float add(float num)
+{
+	printf("%f\n", num);
+	return num;
+}
+
+int main() 
+{
+	//int arr[3];
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	scanf("%d", &arr[i]);
+	//}
+
+	//add_Number(arr);
+	//printf("배열의 각 원소: %d, %d, %d", arr[0], arr[1], arr[2]);
+	add(10);
+	add('A');
+	add(0.05f);
+
+	return 0;
+}
+
+int add_Number(int* parr) 
+{
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		parr[i]++;
+	}
+	return 0;
+}
---
+++ Pointer/Pointer.cpp
@@ -0,0 +1,66 @@
+﻿#include <stdio.h>
+#include "Pointer.h"
+
+void address_operator()
+{
+	int a;
+	a = 2;
+
+	printf("%d \n", a);
+	printf("%p \n", &a);// & 주소연산자
+}
+
+void pointer_operator()
+{
+	int* p;
+	int a;
+	p = &a;
+	a = 2;
+
+	printf("포인터 p에 들어있는 값 : %p \n", p);
+	printf("a의 값 : %d \n", a);
+	printf("p의 값 : %d \n", *p);
+
+	// a변수의 주소를 p 포인터 변수에 대입
+	// * 역참조 연산자로 역참조하여 해당 데이터에 접근 가능
+}
+
+void  swap(int a, int b) 
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+
+int main()
+{
+	//address_operator();
+	//pointer_operator();
+
+	//NewFunction();
+
+	int arr[10] = { 10, 20, 30,40, 50,60 ,70, 80, 90, 100 };
+	int* ptr;
+
+	ptr = arr;
+
+	printf("포인터의 주소:%p\n", ptr);
+	printf("포인터의 값:%d\n", *ptr);
+
+	ptr += 2;
+
+	printf("포인터의 주소:%p\n", ptr);
+	printf("포인터의 값:%d\n", *ptr);
+
+	return 0;
+}
+
+void NewFunction()
+{
+	int a = 5;
+	int b = 10;
+	printf("값 교환 전 : a=%d, b=%d\n", a, b);
+	swap(a, b);
+	printf("값 교환 후 : a=%d, b=%d\n", a, b);
+}
