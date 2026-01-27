261984b64349b130179fca921487b3fbebd2c908
강동윤 <kanginoh@gmail.com>
2026-01-27 16:34:31 +0900

    260127 1634

---
+++ Class/Class.cpp
@@ -0,0 +1,47 @@
+﻿#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+class Student 
+{
+private:
+	char* name;
+	int age;
+	char* hobby;
+public:
+	void ShowInfo();
+	void SetInfo(char* _name, int _age, char* _hobby);
+	void Study();
+	void Sleep();
+};
+
+int main() 
+{
+	Student s;
+	s.SetInfo("Kim", 18, "Game");
+
+	return 0;
+}
+
+void Student::ShowInfo() 
+{
+	cout << "Name: " << name << ", Age: " << age << ", Hobby:" << hobby << std::endl;
+}
+
+void Student::SetInfo(char* _name, int _age, char* _hobby)
+{
+	name = _name;
+	age = _age;
+	hobby = _hobby;
+}
+
+void Student::Study()
+{
+	cout << "STUDY" << endl;
+}
+
+void Student::Sleep()
+{
+	cout << "SLEEP" << endl;
+}
---
+++ Struct/Struct.cpp
@@ -0,0 +1,62 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct Student
+{
+	char name[20];
+	int kor_score;
+	int eng_score;
+	int math_score;
+};
+
+int main()
+{
+	//struct Student *ptr = malloc(sizeof(struct Student));
+	Student* ptr1 = new Student{ "Lee", 66, 76, 86 };
+	Student* ptr2 = new Student{ "Jo", 77, 47, 27 };
+
+
+	printf("%s,%d,%d,%d",
+		ptr1->name,
+		ptr1->kor_score,
+		ptr1->eng_score,
+		ptr1->math_score);
+
+	printf("%s,%d,%d,%d",
+		ptr2->name,
+		ptr2->kor_score,
+		ptr2->eng_score,
+		ptr2->math_score);
+
+	//free(ptr);
+	delete ptr1;
+	delete ptr2;
+
+	return 0;
+}
+
+void whatIsStruct()
+{
+	struct Student s1, s2;
+
+	s1 = { "Kang",90,80,70 };
+	s2 = { "Lee",75,85,65 };
+
+	//strcpy_s(s1.name, "Kang");
+	////st.name = "kang";
+	//s1.kor_score = 90;
+	//s1.eng_score = 80;
+	//s1.math_score = 70;
+
+	printf("%s\n", s1.name);
+	printf("%d\n", s1.kor_score);
+	printf("%d\n", s1.eng_score);
+	printf("%d\n", s1.math_score);
+
+	printf("%s\n", s2.name);
+	printf("%d\n", s2.kor_score);
+	printf("%d\n", s2.eng_score);
+	printf("%d\n", s2.math_score);
+}
