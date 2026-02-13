3ff6fc7327d1532af3e51e9d00fdeef094f99204
강동윤 <kanginoh@gmail.com>
2026-02-13 16:31:35 +0900

    260213 1631

---
+++ Constructor/Constructor.cpp
@@ -0,0 +1,119 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <iostream>
+
+class Marine 
+{
+    int hp;
+    int coord_x, coord_y;
+	int damage;
+    bool is_dead;
+	char* name;
+
+public:
+	Marine(); // 생성자 선언
+	Marine(int x, int y, const char* marine_name); // x, y좌표가 있는 생성자 선언
+	Marine(int x, int y); // x, y좌표가 있는 생성자 선언
+	int attack(); // 공격
+	void be_attacked(int damage_earn); // 입은 데미지
+	void move(int x, int y); // 이동	
+
+	void show_status(); // 상태 출력
+};
+
+Marine::Marine() // 기본 생성자 재정의
+{
+	hp = 50;
+	coord_x = coord_y = 0;
+	damage = 5;
+	is_dead = false;
+	name = NULL;
+}
+
+Marine::Marine(int x, int y) // 매개변수가 있는 생성자 재정의
+{
+	hp = 50;
+	coord_x = x;
+	coord_y = y;
+	damage = 5;
+	is_dead = false;
+	name = NULL;
+}
+
+Marine::Marine(int x, int y, const char* marine_name) // 매개변수가 있는 생성자 재정의
+{
+	name = new char[strlen(marine_name) + 1];
+	strcpy(name, marine_name);
+
+	hp = 50;
+	coord_x = x;
+	coord_y = y;
+	damage = 5;
+	is_dead = false;
+}
+
+void Marine::move(int x, int y)
+{
+	coord_x = x;
+	coord_y = y;
+}
+
+int Marine::attack()
+{
+	return damage;
+}
+
+void Marine::be_attacked(int damage_earn)
+{
+	hp -= damage_earn;
+	if (hp <= 0) is_dead = true;
+}
+
+void Marine::show_status()
+{
+	std::cout << "*** "<< name <<" *** " << std::endl;
+	std::cout << " Location : (" << coord_x << ", " << coord_y << ")" << std::endl;
+	std::cout << " HP : " << hp << std::endl;
+}
+Marine::~Marine() // 소멸자 재정의
+{
+	std::cout << name << " 의 소멸자 호출!" << std::endl;
+
+	if (name != NULL)
+	{
+		delete[] name;
+	}
+}
+
+
+int main()
+{
+	Marine* marines[100];
+
+	marines[0] = new Marine(2, 3, "Marine1"); // 기본 생성자 호출
+	marines[1] = new Marine(10, 20, "Marine2"); // 매개변수가 있는 생성자 호출
+
+	marines[0]->show_status();
+	marines[1]->show_status();
+
+	std::cout << std::endl << "마린1이 마린2를 공격!" << std::endl;
+	marines[1]->be_attacked(marines[0]->attack());
+
+	marines[0]->show_status();
+	marines[1]->show_status();
+
+	int size = sizeof(marines) / sizeof(marines[0]);
+	
+	// ✔ 타입 문맥이면 → 참조자
+	// ✔ 값 앞에 붙으면 → 주소 연산자
+
+	// Marine* ptr = marines[0]; // 주소 연산자 &
+	// Marine*& ref = marines[0]; // 참조자 &
+
+	for(auto &m : marines)
+	{
+		delete m;
+		m = nullptr;
+	}
+}
+
---
+++ PhotonCannon/PhotonCannon.cpp
@@ -0,0 +1,60 @@
+﻿// 포토캐논(복사생성자)
+#include <string.h>
+#include <iostream>
+
+class Photon_Cannon {
+    int hp, shield;
+	int coord_x, coord_y;
+	int damage;
+
+public:
+	Photon_Cannon(int x, int y); // 생성자 선언
+	Photon_Cannon(const Photon_Cannon& pc); // 생성자 선언
+
+	void show_status(); // 상태 출력
+};
+
+Photon_Cannon::Photon_Cannon(const Photon_Cannon& pc) {
+	std::cout << "복사 생성자 호출!" << std::endl;
+	hp = pc.hp;
+	shield = pc.shield;
+	coord_x = pc.coord_x;
+	coord_y = pc.coord_y;
+	damage = pc.damage;
+}
+
+Photon_Cannon::Photon_Cannon(int x, int y) // 생성자 재정의
+{
+	std::cout << "생성자 호출!" << std::endl;
+	hp = shield = 100;
+	coord_x = x;
+	coord_y = y;
+	damage = 20;
+}
+
+void Photon_Cannon::show_status() {
+	std::cout << "Photon Cannon" << std::endl;
+	std::cout << " Location : (" << coord_x << ", " << coord_y << ")" << std::endl;
+	std::cout << "Hp : " << std::endl;
+}
+
+
+int main()
+{
+	Photon_Cannon pc1(3, 3); // 생성자 호출
+	Photon_Cannon pc2(pc1); // 복사 생성자 호출
+	Photon_Cannon pc3 = pc2; // 복사 생성자 호출
+
+	pc1.show_status();
+	pc2.show_status();
+}
+
+//const Photon_Cannon& pc = pc1;
+
+//이건 이런 의미다 :
+//
+//pc1이라는 객체가 있다
+//
+//pc라는 새로운 “접근 경로”를 만든다
+//
+//하지만 그 경로는 읽기 전용이다
