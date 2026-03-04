ab99372aed5752f6c526ee0db066e7e7a6ca6f48
강동윤 <kanginoh@gmail.com>
2026-03-04 16:41:29 +0900

    260304 1641

--- ComplexNumber/ComplexNumber.cpp
+++ ComplexNumber/ComplexNumber.cpp
@@ -16,6 +16,8 @@ public:
     Complex operator/(const Complex& c) const;
     Complex& operator=(const Complex& c);
 
+    void operator+(const char* str);
+
     void println() { std::cout << "( " << real << " , " << img << " ) " << std::endl; }
 };
 
@@ -47,11 +49,81 @@ Complex& Complex::operator=(const Complex& c) {
     return *this;
 }
 
+void Complex::operator+(const char* str) {
+    int length = 0;
+    const char* firstAdress = str;
+    while (1) {
+    
+        if (*firstAdress == '\0') 
+        {
+            length++;
+            break;
+        }
+        length++;
+        firstAdress++; 
+    }
+    // 2-3i\0
+    // 0123 4
+
+    // 3i
+    // 01
+    std::cout << "길이 : " << length << std::endl;
+    int pos = -1;
+
+    for (int i = 0; i < length; i++) {
+        
+        if (*(str + i) == 'i') 
+        {
+            pos = i;
+            break;
+        }
+    }
+    
+    
+    int sum = 0;
+    // 허수부분이 없습니다.
+    if (pos == -1) {
+        std::cout << "실수만 존재 : true" << std::endl;
+
+        int pointPos = -1;
+
+        for (int i = 0; i < length; i++)
+        {
+            if (*(str + i) == '.')
+            {
+                pos = pointPos;
+                break;
+            }
+        }
+
+        if (pointPos == -1) // 정수다(예 53689)
+        {
+            std::cout << "정수 : true" << std::endl;
+            for (int j = 0; j < length - 1; j++)
+            {
+                int temp = (int)(*(str + j)) - '0';
+                std::cout << length - 1 - (j + 1) << "자리 값 : " << temp << std::endl;
+                
+                // j = 0일 때
+                sum += temp * pow(10, length - 1 - (j + 1));
+            }
+        }
+        
+    }
+
+    std::cout << "테스트 : " << sum << std::endl;
+
+    //Complex temp;
+    //return null;
+}
+
 int main() {
     Complex a(1.0, 2.0);
-    Complex b(3.0, -2.0);
-    Complex c(0.0, 0.0);
-    c = a * b + a / b + a + b;
-    c.println();
+    //Complex b(3.0, -2.0);
+    //Complex c(0.0, 0.0);
+    //c = a * b + a / b + a + b;
+    //c.println();
+
+    a + "53689";
 }
 
