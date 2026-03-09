710ad2e033fb73d9bc5db7f73b0dc328c9438ac5
gasshot <kanginoh@gmail.com>
2026-03-09 18:05:21 +0900

    260309 1805

--- DimentionArray/DimentionArray.cpp
+++ DimentionArray/DimentionArray.cpp
@@ -1,23 +1,30 @@
 ﻿#include <iostream>
 
-class DimentionArray {
-
-    int* arrayAdress;
-
-private:
+class DimensionArray {
+    int* arrayAddress;
+    int size;
 
 public:
+    DimensionArray(int array[], int length) {
+        if (length % 2 != 0) {
+            return;
+        }
 
+        int length = sizeof(array) / sizeof(array[0]);
+        arrayAddress = array;
+        size = length;
 
-
+        for (int i = 0; i < size; i++) {
+            std::cout << array[i] << std::endl;
+        }
+    }
 };
 
+int main() {
+    int array[] = { 1, 2, 3, 4, 5 };
+    // sizeof(array)는 main 함수 내에서는 실제 배열 크기를 반환합니다.
+    int length = sizeof(array) / sizeof(array[0]);
 
-
-
-
-int main()
-{
-    std::cout << "Hello World!\n";
+    DimensionArray da(array, length);
 }
 
