422b79cdbead6489c5a3e0b66a67f20c6a665bef
강동윤 <kanginoh@gmail.com>
2026-03-10 16:34:02 +0900

    260310 1634

--- DimentionArray/DimentionArray.cpp
+++ DimentionArray/DimentionArray.cpp
@@ -2,29 +2,32 @@
 
 class DimensionArray {
     int* arrayAddress;
-    int size;
+    int firstDimension;
+    int secondDimension;
+    int thirdDimension;
 
 public:
-    DimensionArray(int array[], int length) {
-        if (length % 2 != 0) {
-            return;
+    DimensionArray(int x1, int x2, int x3) {
+
+        int length = x1 * x2 * x3;
+
+        arrayAddress = new int[length];
+        firstDimension = x1;
+        secondDimension = x2;
+        thirdDimension = x3;
+    }
+
+    int* operator[](int idx){
+        if (idx >= firstDimension || idx < 0) {
+            return nullptr;
         }
 
-        int length = sizeof(array) / sizeof(array[0]);
-        arrayAddress = array;
-        size = length;
-
-        for (int i = 0; i < size; i++) {
-            std::cout << array[i] << std::endl;
-        }
+        return (arrayAddress + idx);
     }
 };
 
 int main() {
-    int array[] = { 1, 2, 3, 4, 5 };
-    // sizeof(array)는 main 함수 내에서는 실제 배열 크기를 반환합니다.
-    int length = sizeof(array) / sizeof(array[0]);
 
-    DimensionArray da(array, length);
+    return 0;
 }
 
