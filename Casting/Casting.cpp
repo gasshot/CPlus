6d22d3a5cbfc7126737500d5a071c9791c323caf
강동윤 <kanginoh@gmail.com>
2026-03-11 16:32:47 +0900

    260311 1632

---
+++ Casting/Casting.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a = 156;
+	float b = 0.0f;
+
+	b = static_cast<float> (a);
+
+	a = static_cast<int>(b - 50.7f);
+
+
+	printf("%f\n", b);
+	printf("%d", a);
+
+	return 0;
+
+}
--- DimentionArray/DimentionArray.cpp
+++ DimentionArray/DimentionArray.cpp
@@ -2,31 +2,88 @@
 
 class DimensionArray {
     int* arrayAddress;
-    int firstDimension;
-    int secondDimension;
-    int thirdDimension;
+    int* checkDimension;
+    int length;
 
 public:
-    DimensionArray(int x1, int x2, int x3) {
+    DimensionArray(int x1) {
 
-        int length = x1 * x2 * x3;
+        length = x1;
+        arrayAddress = new int[length];
+        checkDimension = nullptr;
+
+        for (int i = 0; i < length; i++)
+        {
+            int* ptr = (arrayAddress + i);
+            *ptr = i;
+            //printf("%p\n", arrayAddress + i);
+            printf("%d\n", *ptr);
+        }
+    }
+
+    DimensionArray(int x1, int x2) {
+
+        length = x1 * x2;
 
         arrayAddress = new int[length];
-        firstDimension = x1;
-        secondDimension = x2;
-        thirdDimension = x3;
+        checkDimension = new int[2];
+        checkDimension[0] = x1;
+        checkDimension[1] = x2;
+
+        for (int i = 0; i < length; i++)
+        {
+            printf("%p\n", arrayAddress + i);
+        }
     }
 
     int* operator[](int idx){
-        if (idx >= firstDimension || idx < 0) {
+        if (idx < 0) {
             return nullptr;
         }
 
-        return (arrayAddress + idx);
+        if (checkDimension == nullptr) 
+        {
+            // 1차원 배열일 때
+            if (idx > length - 1) {
+                std::cout << "범위를 벗어났습니다." << std::endl;
+                return nullptr;
+            }
+            return (arrayAddress + idx);
+        }
+
+        else {
+            // 2차원 배열일 때: idx행의 '시작 주소'를 반환
+            // (idx * 열의 개수) 위치의 주소를 주면, 
+            // 외부에서 [j]를 사용했을 때 그 주소로부터 j만큼 더 이동하게 됨
+            int columnCount = checkDimension[1];
+            return (arrayAddress + (idx * columnCount));
+        }
+
+        // [0][1][2][3][4][5][6][7]
+        // [0][0][0] = [0]
+        // [0][0][1] = [1]
+        // [0][1][0] = [2]
+        // [0][1][1] = [3]
+        // [1][0][0] = [4]
+        // [1][0][1] = [5]
+        // [1][1][0] = [6]
+        // [1][1][1] = [7]
+
+
+    }
+    ~DimensionArray() {
+        delete[] arrayAddress;
+        delete[] checkDimension;    
     }
 };
 
+
 int main() {
+
+    //DimensionArray arr(8);
+    //std::cout << *(arr[9]) << std::endl;
+
+    DimensionArray arr(2,2);
 
     return 0;
 }
